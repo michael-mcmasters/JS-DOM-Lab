07ada3b31388a54ec9deb3df9881edcf5b2080a5
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-04 21:12:29 -0500

    Add Bootstrap dropdown menu

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const button = document.getElementById("calculate-btn");
 button.addEventListener("click", () => {
   console.log("jfdksl");
 });
+
+const dropDownMenu = document.getElementById("dropdownMenu2");
+dropDownMenu.innerText = "Fahrenheit";
