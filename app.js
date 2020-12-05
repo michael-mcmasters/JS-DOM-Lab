896a76111980de159837f18135adf38084d629ed
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-04 22:16:34 -0500

    Create enum for temperature units

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
 const button = document.getElementById("calculate-btn");
+const dropDownItems = document.getElementsByClassName("dropdown-item");
+
+// Enum
+let Temperature = {
+  Fahraenheit: 1,
+  Celcius: 2,
+  Kelvin: 3,
+  Rankine: 4,
+}
+
 button.addEventListener("click", () => {
-  const valueElement = document.getElementById("value");
-  const celcius = calculate(valueElement.value);
-  setValue(celcius);
+  calculate();
 });
 
 document.body.addEventListener("keypress", (event) => {
-  if (event.key === "Enter")
-    console.log("ayyoo");
+  if (event.key === "Enter") {
+    calculate();
+  }
 });
 
-const dropDownItems = document.getElementsByClassName("dropdown-item");
 for (let i = 0; i < dropDownItems.length; i++) {
   dropDownItems[i].addEventListener("click", (event) => {
     console.log(event.target);
   })
 }
 
+
+
+
+function calculate() {
+  const valueElement = document.getElementById("value");
+  const celcius = convertToFahrenheit(Temperature.Fahraenheit, valueElement.value);
+  setValue(celcius);
+}
+
+function convertToFahrenheit(temperature, value) {
+  if (temperature === Temperature.Fahraenheit) {
+    return (value - 32) * 5/9;
+  }
+  console.log(Temperature);
+}
+
+function convertToCelsius() {
+  
+}
+
+function convertToKelvin() {
+  
+}
+
+function convertToRankine() {
+  
+}
+
+function setValue(celcius) {
+  console.log(celcius);
+}
+
 function setDropDown() {
   const dropDownMenu = document.getElementById("dropdownMenu2");
   dropDownMenu.innerText = "Fahraenheit";
-}
-
-function calculate(value) {
-  return (value - 32) * 5/9;
-}
-
-function setValue() {
-  console.log(celcius);
 }
