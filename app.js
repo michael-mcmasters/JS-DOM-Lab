f6fba55b0f405260fdd0cf512d21c4b03fdc7772
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-04 21:57:17 -0500

    Convert Farenheit to celcius

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const button = document.getElementById("calculate-btn");
 button.addEventListener("click", () => {
-  console.log("jfdksl");
+  const valueElement = document.getElementById("value");
+  const celcius = calculate(valueElement.value);
+  setValue(celcius);
 });
-
-const dropDownMenu = document.getElementById("dropdownMenu2");
-dropDownMenu.innerText = "Fahraenheit";
-
 
 const dropDownItems = document.getElementsByClassName("dropdown-item");
 for (let i = 0; i < dropDownItems.length; i++) {
   dropDownItems[i].addEventListener("click", (event) => {
     console.log(event.target);
   })
+}
+
+function setDropDown() {
+  const dropDownMenu = document.getElementById("dropdownMenu2");
+  dropDownMenu.innerText = "Fahraenheit";
+}
+
+function calculate(value) {
+  return (value - 32) * 5/9;
+}
+
+function setValue() {
+  console.log(celcius);
 }
